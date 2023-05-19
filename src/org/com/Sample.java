5b7dbaee00b9c671927d2b74cc4ba2c0fd122e25
AbsaraAbs <absaraabs09@gmail.com>
2023-05-19 19:13:44 +0530

    project updated

---
+++ src/org/com/Sample.java
@@ -0,0 +1,15 @@
+package org.com;
+
+public class Sample {
+	public static void main(String[] args) {
+		
+	int i =10;
+	int i1 =55;
+	System.out.println(i+i1);
+	String s="Flowers";
+	String s1="Birds";
+	System.out.println(s);
+	System.out.println(s1);
+	
+	}
+}
